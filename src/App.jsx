4a25a07c131848c15828009fe9f7a3357a40babc
Madeline Collier <madel@telus.net>
2017-07-24 23:28:33 +0000

    created initial page structure in jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import Message from './Message.jsx';
+import MessageList from './MessageList.jsx';
+import NavBar from './NavBar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+          <NavBar></NavBar>
+          <Message></Message>
+          <MessageList></MessageList>
+          <ChatBar></ChatBar>
+      </div>
     );
   }
 }
