07311e1daeee92a06c80d2bb8a69db36c0c6dbd6
Madeline Collier <madel@telus.net>
2017-07-28 18:50:58 +0000

    finalized styling, added screenshots to docs folder

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,7 +4,7 @@ class NavBar extends Component {
   render() {
     return (
       <nav className='navbar'>
-        <a href='/' className='navbar-brand'>Chatty</a>
+        <a href='/' className='navbar-brand'>CHATTY</a>
         <span className='client-counter'>{ this.props.clients } users online</span>
       </nav>
     );
